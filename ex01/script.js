ae2f2a45935b487b19c92e69e2da167587516321
Luizgsmkw <luizgustavokw@gmail.com>
2022-06-13 16:28:54 -0300

    first commit

---
+++ ex01/script.js
@@ -0,0 +1,15 @@
+// Crie uma variável que receba um nome;
+let nome = "Luiz Gustavo";
+// Crie uma variável que receba um número;
+let numero = 26;
+// Crie uma variável com o ano atual e subtraia pelo número que você guardou;
+let anoAtual = 2022 - numero;
+// Agora mostre no console o resultado da soma das duas variáveis guardadas anteriormente;
+console.log(numero + anoAtual);
+//Mostre no console a mensagem "Estou mexendo no console.log";
+console.log("Estou mexendo no console.log")
+// Crie uma variável chamada “quartaFeira” e atribua à ela o valor booleano que representa verdadeiro ou falso e mostre a saída no console;
+let quartaFeira = false;
+console.log(quartaFeira);
+// Exiba no console o tipo de dado da váriavel quartaFeira;
+console.log(typeof quartaFeira);
