a4bd11fe04d234dcd5b3a3658c877cf8e923638e
Luizgsmkw <luizgustavokw@gmail.com>
2022-06-13 17:17:47 -0300

    Atualização do ex02

--- ex02/script.js
+++ ex02/script.js
@@ -4,26 +4,32 @@ if(idade > 18) {
     console.log("Você é maior de idade")
 } 
 //2 - Crie uma condição que exiba uma mensagem no console se você for maior de idade E a condição humana seja true
-let idade2 = 27;
+let idade2 = 26;
 let humano = true;
-if(idade > 18 && humano === true) {
+if(idade2 > 18 && humano === true) {
     console.log("Você é maior de idade e pertence a raça humana");
 }
 
 //3 - Criar uma condição que exiba uma mensagem no console se você fizer aniversário em Janeiro OU Dezembro
-let aniversario = "dezembro";
+let aniversario = "novembro";
 if(aniversario === "dezembro" || aniversario === "janeiro") {
     console.log("Parabéns seu aniversário é esse mês ");
+} else {
+    console.log("Você não faz aniversário nem em janeiro nem em dezembro ")
 }
 //*** Exercícios bônus
 //4 - Criar uma condição que exiba uma mensagem no console se seu nome começar com a letra R
-let nome = "Roberto";
+let nome = "Luiz";
 if(nome[0] === "R") {
     console.log("Seu nome começa com a letra R")
+} else {
+    console.log("Seu nome não começa com a letra R")
 }
 //5 - Criar uma condição que exiba uma mensagem no console se seu sobrenome tenha mais de 6 letras OU seu nome começar com a letra E
 let nome2 = "Luiz";
 let sobrenome = "Gustavo"
 if(sobrenome.length > 5 || nome2[0] === "E" ) {
     console.log("Seu sobronome tem mais do que 6 letras ou seu nome começa com E")
+} else {
+    console.log("Seu sobronome não têm mais do que 6 letras e seu nome não começa com E")
 }
