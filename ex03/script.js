3e3966ae007736be00b1eebcd1d0b0fb6dd14939
Luizgsmkw <luizgustavokw@gmail.com>
2022-06-13 21:45:52 -0300

    Realização do ex03

---
+++ ex03/script.js
@@ -0,0 +1,28 @@
+
+//1 - Criar um loop que conte de 1 até 10 usando FOR
+
+for (let contador = 1; contador <= 10; contador++) {
+    console.log(contador)
+}
+
+//2 - Criar um loop que conte de 10 até 1 usando WHILE
+
+let contador2 = 10;
+while (contador2 >= 1) {
+    console.log(contador2)
+    contador2--
+}
+
+//3 - Criar um loop que conte todos os números ímpares de 1 até 100
+
+for (let contador3 = 1; contador3 <= 100; contador3 += 2) {
+    console.log(contador3)
+}
+
+//4- Criar um loop que conte todos os números pares de 0 a 100
+
+let contador4 = 0;
+while (contador4 <= 100) {
+    console.log(contador4)
+    contador4 += 2;
+}
