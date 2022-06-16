a75180512c5063ddff069e62ff0de1bb0e6ffa08
Luizgsmkw <luizgustavokw@gmail.com>
2022-06-16 14:55:40 -0300

    Realização do ex04

---
+++ ex04/script.js
@@ -0,0 +1,31 @@
+//1 - crie uma função que exiba uma mensagem no console
+
+function mensagem() {
+    console.log("Olá, mundo!!")
+}
+
+mensagem()
+
+//2 - crie uma função que receba o seu nome como (parâmetro) e exiba no console
+
+function seuNome(nome) {
+    console.log(nome)
+}
+
+seuNome("Luiz")
+
+//3 - crie uma função que receba nome, idade, e um estilo musical preferido (parâmetros) e exiba no console
+
+function info(nome, idade, estiloMusical) {
+    console.log(nome, idade, estiloMusical)
+}
+
+info("Luiz", 26, "MPB")
+
+//4 - crie uma função que receba um filme, uma música (parâmetros) e exiba no console
+
+function cultura(filme, musica,) {
+    console.log(filme, musica)
+}
+
+cultura("Aquarius", "Meu Caro Amigo - Chico Buarque")
