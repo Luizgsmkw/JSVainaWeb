0d727c1d5103957fb4c3b97c8c886c3c4058ac38
Luizgsmkw <luizgustavokw@gmail.com>
2022-06-23 11:44:34 -0300

    Realização do ex05

---
+++ ex05/script.js
@@ -0,0 +1,31 @@
+// Crie um array que receba 5 itens e exiba no console.
+
+let nomes = ["Luiz", "Carlos", "Joana", "Maria", "Fernanda"]
+console.log(nomes)
+
+// Utilize um método para adicionar um nome ao inicio do array.
+
+nomes.unshift("Andre")
+console.log(nomes)
+
+// Utilize um método para remover o último nome do array.
+
+nomes.pop()
+console.log(nomes)
+
+// Utilize um método para adicionar dois nomes ao fim do array.
+
+nomes.push("Carla", "João")
+console.log(nomes)
+
+// Utilize um método para remover o primeiro nome do array.
+
+nomes.shift()
+console.log(nomes)
+
+// Utilize um método para organizar em ordem crescente o seguinte array:
+// let numbers = [7,5,6,3,8,9,2,1,4]
+
+let numbers = [7,5,6,3,8,9,2,1,4]
+numbers.sort()
+console.log(numbers)
